45f0ec5f2f4b496b33e70899a695fe4655131447
Steven <saanderson1987@gmail.com>
2017-05-16 15:22:52 -0400

    fix end game bug

--- bundle.js
+++ bundle.js
@@ -146,14 +146,17 @@ class Piece {
     let gameOver = true;
     for (let i = 0; i < this.piecePos.length; i++) {
       let piecePo = this.piecePos[i];
-      if (piecePo[0] === 19
+      if (piecePo[0] === -1
+        && $( `li[pos='${piecePo[0]+1},${piecePo[1]}']` ).attr("static") === "true"
+      ){
+        console.log('game over man');
+        return "game over";
+      }
+      else if (piecePo[0] === 19
         || $( `li[pos='${piecePo[0]+1},${piecePo[1]}']` ).attr("static") === "true"
       ){
         return "make static";
-      } else if (piecePo[0] === -1
-        && $( `li[pos='${piecePo[0]+1},${piecePo[1]}']` ).attr("static") === "true"
-      )
-        return "game over";
+      }
     }
 
 
@@ -502,10 +505,10 @@ class IPiece extends Piece  {
   setPos() {
 
     this.rotationKeys = [
-      [ [-1, 3], [-1, 4], [-1,5], [-1,6] ],
-      [ [-3, 4], [-2, 4], [-1, 4], [0, 4] ],
-      [ [-2, 3], [-2, 4], [-2,5], [-2,6] ],
-      [ [-3, 5], [-2, 5], [-1, 5], [0, 5] ]
+      [ [-3, 3], [-3, 4], [-3,5], [-3,6] ],
+      [ [-5, 4], [-4, 4], [-3, 4], [-2, 4] ],
+      [ [-4, 3], [-4, 4], [-4,5], [-4,6] ],
+      [ [-5, 5], [-4, 5], [-3, 5], [-2, 5] ]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
@@ -542,10 +545,10 @@ class JPiece extends Piece  {
 
   setPos() {
     this.rotationKeys = [
-      [ [-1,3], [-1,4], [-1,5], [0,5] ],
-      [ [0,3], [-2,4], [-1,4], [0,4]  ],
-      [ [-2,3], [-1,3], [-1,4], [-1,5] ],
-      [ [-2,4], [-1,4], [0,4], [-2,5] ]
+      [ [-3,3], [-3,4], [-3,5], [-2,5] ],
+      [ [-2,3], [-4,4], [-3,4], [-2,4]  ],
+      [ [-4,3], [-3,3], [-3,4], [-3,5] ],
+      [ [-4,4], [-3,4], [-2,4], [-4,5] ]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
@@ -587,10 +590,10 @@ class LPiece extends Piece  {
   setPos() {
 
     this.rotationKeys = [
-      [ [0,3], [-1,3], [-1,4], [-1,5] ],
-      [ [-2,3], [-2,4], [-1,4], [0,4] ],
-      [ [-1,3], [-1, 4], [-1, 5], [-2, 5] ],
-      [ [-2,4], [-1,4], [0, 4], [0, 5] ]
+      [ [-2,3], [-3,3], [-3,4], [-3,5] ],
+      [ [-4,3], [-4,4], [-3,4], [-2,4] ],
+      [ [-3,3], [-3, 4], [-3, 5], [-4, 5] ],
+      [ [-4,4], [-3,4], [-2, 4], [-2, 5] ]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
@@ -624,7 +627,7 @@ class OPiece extends Piece  {
   }
 
   setPos() {
-    this.piecePos = [ [0, 4], [-1, 4], [0, 5], [-1, 5] ];
+    this.piecePos = [ [-2, 4], [-3, 4], [-2, 5], [-3, 5] ];
     this.colorPos();
   }
 
@@ -662,10 +665,10 @@ class SPiece extends Piece  {
   setPos() {
 
     this.rotationKeys = [
-      [ [-1,3], [-1,4], [-2,4], [-2,5] ],
-      [ [-1,4], [-2,4], [0,5], [-1,5] ],
-      [ [0,3], [0,4], [-1,4], [-1,5] ],
-      [ [-2,3], [-1,3], [-1,4], [0,4] ]
+      [ [-3,3], [-3,4], [-4,4], [-4,5] ],
+      [ [-3,4], [-4,4], [-2,5], [-3,5] ],
+      [ [-2,3], [-2,4], [-3,4], [-3,5] ],
+      [ [-4,3], [-3,3], [-3,4], [-2,4] ]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
@@ -722,10 +725,10 @@ class TPiece extends Piece  {
   setPos() {
 
     this.rotationKeys = [
-      [ [-1, 3], [-2,4], [-1,4], [-1,5] ],
-      [ [-1,4], [-2,4], [0,4], [-1,5] ],
-      [ [-1,3], [-1,4], [0,4], [-1,5] ],
-      [ [-1,3], [-2,4], [0,4], [-1, 4]]
+      [ [-3, 3], [-4,4], [-3,4], [-3,5] ],
+      [ [-3,4], [-4,4], [-2,4], [-3,5] ],
+      [ [-3,3], [-3,4], [-2,4], [-3,5] ],
+      [ [-3,3], [-4,4], [-2,4], [-3, 4]]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
@@ -766,10 +769,10 @@ class ZPiece extends Piece  {
   setPos() {
 
     this.rotationKeys = [
-      [ [-2,3], [-2,4], [-1,4], [-1,5] ],
-      [ [-1,4], [-2,5], [0,4], [-1,5] ],
-      [ [-1,3], [-1,4], [0,4], [0,5]],
-      [ [-1,3], [-2,4], [0,3], [-1,4]]
+      [ [-4,3], [-4,4], [-3,4], [-3,5] ],
+      [ [-3,4], [-4,5], [-2,4], [-3,5] ],
+      [ [-3,3], [-3,4], [-2,4], [-2,5]],
+      [ [-3,3], [-4,4], [-2,3], [-3,4]]
     ];
 
     this.piecePos = JSON.parse(JSON.stringify(this.rotationKeys[this.pieceLayout]));
